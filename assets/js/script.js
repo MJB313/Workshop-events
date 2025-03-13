accf9ca70810dbc815195034eda4b228ad950a9d
MJB313 <10632186@ucn.dk>
2025-03-13 12:44:01 +0100

    Opgave 1 og 2 færdig

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 /*1. Anvende javascript til at skifte farven i de sociale elementer til en mere passende farve (jeres vurdering) */
 
+const socialEl1 = document.querySelector(".fa-twitter").style.color="#1DA1F2";
+const socialEl2 = document.querySelector(".fa-facebook").style.color="#1877F2";
+const socialEl3 = document.querySelector(".fa-instagram").style.color="#e1306c";
+
 /*2. Se den lidt skæve h1 i header - sørg for den ved hover "shaker"/ryster sig lidt. I bedes anvende javascript til at løse denne opgave - måske i kombination med css. Der findes endda js libraries/biblioteker I kan se på til formålet, f.eks. anime.js */
+//Mads
 
-//Mads laver de 2 første
+const h1El = document.querySelector("h1");
 
-
-
-
+h1El.classList.add("hover");
 
 
 
@@ -21,8 +24,9 @@
 /*3. Brug javascript til at skifte billedet ud i .bigPicture-boksen, brug dette eller noget helt andet: https://cdn.pixabay.com/photo/2017/09/28/13/18/amusement-2795490_960_720.jpg */
 
 /*4. Brug javascript til at indsætte et favicon - f.eks. det her http://hearthstoneapi.com/favicon.ico (I know...hvorfor ville man nogensinde gøre det irl...men for øvelsens skyld) */
+//Jan
 
-// Jan laver opgave 3 og 4. 
+
 
 
 
@@ -47,32 +51,9 @@
 
 /*5.2. Sørg for at når man trykker på "Læs mere..." vises den oprindelige tekst ovenover "Læs mere..."  */
 
-// Tobias tager 5.1 og 5.2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*5.3. Sørg for at "Læs mere..." bliver lavet om til "Læs mindre..." */
 
+
+
+
 /*6. Som I måske har lagt mærke til, er der en .crazyPopup box som står udenfor body (se i css...prøv f.eks. at ændre property "right" til value -200 og 0). Opgaven er at den nu "popper up" når personer har scrollet 100px ned og forsvinde igen når de scroller op under 100px igen  - lookie: https://drive.google.com/file/d/10aSqzcL3br8DU5AhggE54Pal0Z3s-x5y/view */
-
-
-// Kasper tager 5.3 og 6
