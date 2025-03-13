23fe6ec532432a33bedcc8b9fda0afeb7fc1c960
Kasper4113 <Kaspervalstedpetersen@gmail.com>
2025-03-13 12:44:49 +0100

    opg 6
    opg 6

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,16 @@
 
 
 
+
+
+
+
+
+
+
 /*5.1 Brug javascript til at gemme teksterne væk i artiklerne <p>-tags. Indsæt istedet link med "Læs mere..." */
+
+
 
 /*5.2. Sørg for at når man trykker på "Læs mere..." vises den oprindelige tekst ovenover "Læs mere..."  */
 
@@ -51,28 +60,66 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*5.3. Sørg for at "Læs mere..." bliver lavet om til "Læs mindre..." */
+
+
+
+
+
+
 
 /*6. Som I måske har lagt mærke til, er der en .crazyPopup box som står udenfor body (se i css...prøv f.eks. at ændre property "right" til value -200 og 0). Opgaven er at den nu "popper up" når personer har scrollet 100px ned og forsvinde igen når de scroller op under 100px igen  - lookie: https://drive.google.com/file/d/10aSqzcL3br8DU5AhggE54Pal0Z3s-x5y/view */
 
 
+
+
+
+
+
+
+
+
+
+const crazyPop = document.querySelector(".crazyPopup");
+
+window.addEventListener("scroll", function () {
+    if (this.window.scrollY >= 100) {
+            crazyPop.style.right = 0;
+        }   else {
+            crazyPop.style.right = "-400px";
+        }
+    })
+
 // Kasper tager 5.3 og 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     popup.innerHTML = "<p>You've scrolled 100px!</p><button onclick='this.parentElement.style.display="none"'>Close</button>";
+// popup.style.position = "fixed";
+// popup.style.bottom = "20px";
+// popup.style.right = "20px";
+// popup.style.borderRadius = "5px";
+// popup.style.display = "none";
+// document.body.appendChild(popup);
+
+// window.addEventListener("scroll", function () {
+//     if (window.scrollY >= 100) {
+//         popup.style.display = "block";
+//     }
+// });
